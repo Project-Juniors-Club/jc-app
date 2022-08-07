63648a46fae2fa803a2e18b7a6775cbdc2710b85
kevin chua <skullspine9@gmail.com>
2022-08-07 10:06:06 +0800

    fix: deploy

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -32,6 +32,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
     params: { id: user.id.toString() },
   }));
 
+  console.log('HI');
   // We'll pre-render only these paths at build time.
   // { fallback: false } means other routes should 404.
   return { paths, fallback: false };
