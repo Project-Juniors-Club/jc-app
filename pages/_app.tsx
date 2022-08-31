9c99de4c2541c05672da856433961f7c6972b775
kevin chua <skullspine9@gmail.com>
2022-08-31 11:29:44 +0800

    feat: upgrade typescript, add chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,27 @@ import React from 'react';
 import { AppProps } from 'next/app';
 
 import '../styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
+
+import { extendTheme } from '@chakra-ui/react';
+
+// TODO: add theme when we have the UI
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+};
+
+const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
