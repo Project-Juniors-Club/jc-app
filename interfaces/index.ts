ddbfae45c152a94e7e147d4e7a5481da9de84af0
BryannYeap <e0543723@u.nus.edu>
2022-09-04 14:18:51 +0800

    feat: set up prisma schema for database and define interfaces based on schema

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -4,18 +4,144 @@
 //
 // import { User } from 'path/to/interfaces';
 
+export enum UserType {
+  normalUser,
+  admin,
+  superAdmin
+}
+
 export type User = {
-  id: number;
-  name: string;
+  id: string;
+  username: string;
+  password: string;
+  email: string;
+  type: UserType;
 };
 
-export type Post = {
+export type NormalUser = {
   id: string;
-  title: string;
-  content: string;
-  published: boolean;
-  authorId: string;
-  author: {
+  userId: string;
+  permissionLevel: number;
+  user: {
+    username: string
+  }
+}
+
+
+export type Admin = {
+  id: string;
+  userId: string;
+  permissionLevel: number;
+  user: {
+    username: string
+  }
+}
+
+
+export type SuperAdmin = {
+  id: string;
+  userId: string;
+  permissionLevel: number;
+  user: {
+    username: string
+  }
+}
+
+export type UserCourse = {
+  id: string;
+  userId: string;
+  courseId: string;
+  progress: number;
+  correctQns: number;
+  stars: number;
+  user: {
+    username: string
+  }
+  course: {
+    name: string
+  }
+}
+
+export type Course = {
+  id: string;
+  name: string;
+  description: string;
+  stars: number;
+  adminId: string;
+}
+
+export enum CourseItemType {
+  game,
+  image,
+  video,
+  article
+}
+
+export type CourseItem = {
+  id: string;
+  name: string;
+  description: string;
+  pageNumber: number;
+  courseId: string;
+  type: CourseItemType;
+  course: {
     name: string;
-  };
-};
+  }
+}
+
+export type Image = {
+  id: string;
+  courseItemId: string;
+  url: string;
+  courseItem: {
+    name: string;
+  }
+}
+
+export type Video = {
+  id: string;
+  courseItemId: string;
+  url: string;
+  courseItem: {
+    name: string;
+  }
+}
+
+export type Article = {
+  id: string;
+  courseItemId: string;
+  text: string;
+  courseItem: {
+    name: string;
+  }
+}
+
+export enum GameType {
+  spotTheDifferenceGame,
+  matchingGame,
+  sortingGame,
+}
+
+export type Game = {
+  id: string;
+  courseItemId: string;
+  type: GameType
+  courseItem: {
+    name: string;
+  }
+}
+
+export type SpotTheDifferenceGame = {
+  id: string;
+  gameId: string;
+}
+
+export type MatchingGame = {
+  id: string;
+  gameId: string;
+}
+
+export type SortingGame = {
+  id: string;
+  gameId: string;
+}
