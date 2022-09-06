22ec9f9dfec94f911ae9b10e725fc456818f85d4
Stuart Long Chay Boon <chayboon@gmail.com>
2022-09-06 17:58:15 +0800

    fix: remove commented out code

--- components/RouteGuard.tsx
+++ components/RouteGuard.tsx
@@ -12,21 +12,6 @@ export function RouteGuard({ children }) {
     useEffect(() => {
         // on initial load - run auth check 
         authCheck(router.asPath);
-
-        // // on route change start - hide page content by setting authorized to false  
-        // const hideContent = () => setAuthorized(false);
-        // router.events.on('routeChangeStart', hideContent);
-
-        // // on route change complete - run auth check 
-        // router.events.on('routeChangeComplete', authCheck)
-
-        // // unsubscribe from events in useEffect return function
-        // return () => {
-        //     router.events.off('routeChangeStart', hideContent);
-        //     router.events.off('routeChangeComplete', authCheck);
-        // }
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     function authCheck(url) {
