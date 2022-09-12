e6addbd6e716f3485aace57b2452fb91d58225e7
Stuart Long Chay Boon <chayboon@gmail.com>
2022-09-13 02:09:38 +0800

    fix: mute recoil warning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,6 +19,30 @@ const colors = {
 
 const theme = extendTheme({ colors });
 
+const memoize = (fn) => {
+  let cache = {};
+  return (...args) => {
+    let n = args[0];
+    if (n in cache) {
+      return cache[n];
+    }
+    else {
+      let result = fn(n);
+      cache[n] = result;
+      return result;
+    }
+  }
+}
+
+// ignore in-browser next/js recoil warnings until its fixed.
+const mutedConsole = memoize((console) => ({
+  ...console,
+  warn: (...args) => args[0].includes('Duplicate atom key')
+    ? null
+    : console.warn(...args)
+}))
+global.console = mutedConsole(global.console);
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
