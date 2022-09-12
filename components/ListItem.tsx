049e8a6b8e8bd0de622b14e63449ace8b2387292
sltsheryl <sltsheryl@gmail.com>
2022-09-12 22:06:16 +0800

    Fix merge conflict

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -8,13 +8,9 @@ type Props = {
 }
 
 const ListItem = ({ data }: Props) => (
-  <Link href="/users/[id]" as={`/users/${data.pk}`}>
+  <Link href="/users/[id]" as={`/users/${data.id}`}>
     <a>
-<<<<<<< HEAD
-      {data.pk}: {data.username}
-=======
       {data.id}: {data.username} ({data.email})
->>>>>>> f70600e5c640e4f1c38e953a4570116dd57c3b70
     </a>
   </Link>
 )
