6ea800e2d472e2ef2edc58fdf72da7005bc86a6e
kevin <skullspine9@gmail.com>
2022-10-04 19:38:41 +0800

    style: minor changes

--- utils/api-messages.ts
+++ utils/api-messages.ts
@@ -1,15 +1,15 @@
 const messages = {
-    getOneImageSuccess: 'Successfully retrieved image.',
-    getAllImagesSuccess: 'Successfully retrieved images.',
-    createImageSuccess: 'Successfully created image(s).',
-    updateImageSuccess: 'Successfully updated image(s).',
-    deleteImageSuccess: 'Successfully deleted image(s).',
-    
-    getOneImageFailed: 'Error in retrieving image.',
-    getAllImagesFailed: 'Error in retrieving images.',
-    createImageFailed: 'Error in creating image(s).',
-    updateImageFailed: 'Error in updating image(s).',
-    deleteImageFailed: 'Error in deleting image(s).',
-}
+  getOneImageSuccess: 'Successfully retrieved image.',
+  getAllImagesSuccess: 'Successfully retrieved images.',
+  createImageSuccess: 'Successfully created image(s).',
+  updateImageSuccess: 'Successfully updated image(s).',
+  deleteImageSuccess: 'Successfully deleted image(s).',
+
+  getOneImageFailed: 'Error in retrieving image.',
+  getAllImagesFailed: 'Error in retrieving images.',
+  createImageFailed: 'Error in creating image(s).',
+  updateImageFailed: 'Error in updating image(s).',
+  deleteImageFailed: 'Error in deleting image(s).',
+};
 
 export default messages;
