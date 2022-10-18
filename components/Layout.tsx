fc6e585b69939b1b0b27a9dfc6ac5fac4b868291
jengoc415 <jengoc415@gmail.com>
2022-10-18 23:32:24 +0800

    API for CRUD completed. Separated view and upload into 2 separate routes under uploads page.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -27,10 +27,8 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
         <Link href='/users'>
           <a>Users List</a>
         </Link>{' '}
-        | <Link href='/api/users'>Users API</Link>
-        {' '}
-        |{' '}
-        <Link href='login'>Login</Link>
+        | <Link href='/api/users'>Users API</Link> | <Link href='/login'>Login</Link> | <Link href='/uploads'>View Uploads</Link> |{' '}
+        <Link href='/uploads/upload'>Upload</Link>
       </nav>
     </header>
     {children}
---
+++ pages/uploads/index.tsx
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import Link from 'next/link';
+import { useState } from 'react';
+import Layout from '../../components/Layout';
+
+const Uploads = ({ url: url }) => {
+  const [file, setFile] = useState<File>();
+  const [message, setMessage] = useState<String>();
+  console.log(url);
+
+  function storeFile(event) {
+    setFile(event.target.files[0]);
+    setMessage(null);
+  }
+
+  return (
+    <Layout title='Upload Media'>
+      <p className='container flex p-4'>These are the files in juniors club AWS bucket. Not completed.</p>
+      {/* <Image src={data} alt='data' /> */}
+    </Layout>
+  );
+};
+
+// export async function getServerSideProps(context) {
+//   const { data } = await axios.get('/api/media/');
+
+//   return {
+//     props: {
+//       url: data.url,
+//     },
+//   };
+// }
+
+export default Uploads;
