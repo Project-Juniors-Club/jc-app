a91ff4181f2e870c8c44f0f5711820755abfd327
Remus <remuskwan23@gmail.com>
2022-10-18 22:22:26 +0800

    refactor: removed some commented out code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,7 @@
-import { useSession } from 'next-auth/react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 import Layout from '../components/Layout';
 
 const IndexPage = () => {
-  // const { data: session } = useSession();
-  // const router = useRouter();
-
-  // useEffect(() => {
-  //   if (!session) {
-  //     router.push('/login');
-  //     return;
-  //   }
-  // }, [session, router]);
-
   return (
     <Layout title='Home | Next.js + TypeScript Example'>
       <h1>Hello Next.js 👋</h1>
