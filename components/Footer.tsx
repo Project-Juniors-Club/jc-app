884282572bffb1eaff606b5dc45d4a0e446281b3
kxrt <kvrtikeya@gmail.com>
2022-12-04 02:36:28 +0800

    task(footer): add social icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,13 +4,13 @@ import { Box, Flex, Link, Stack, Text } from '@chakra-ui/react';
 const Footer = () => {
   return (
     <footer>
-      <Box as='footer' role='contentinfo' mx='auto' py='4' px='24' bgColor='#262728' color='white' fontSize='sm' fontFamily='Open Sans'>
-        <Stack>
-          <Text fontWeight='900' fontSize='2xl' mb='30px'>
+      <Box as='footer' role='contentinfo' mx='auto' py='4' px='24' bgColor='#262728' color='gray.200' fontSize='md'>
+        <Stack fontSize='md'>
+          <Text fontWeight='900' fontSize='2xl' mb='30px' mt='20px'>
             CONTACT US
           </Text>
           <Flex direction={{ sm: 'column', md: 'column', lg: 'row' }} align='center' justify={{ base: 'center', md: 'space-between' }}>
-            <Box h='100px' w='250px'>
+            <Box h='100px' w='270px'>
               <Text fontWeight='700' mb='20px'>
                 Donor, Media &amp; General Enquiries
               </Text>
@@ -21,7 +21,7 @@ const Footer = () => {
                 <Text>Tel: +65 9855 4805</Text>
               </Link>
             </Box>
-            <Box h='100px' w='250px'>
+            <Box h='100px' w='270px'>
               <Text fontWeight='700' mb='20px'>
                 Volunteers
               </Text>
@@ -29,7 +29,7 @@ const Footer = () => {
                 <Text>Contact: volunteer@foodbank.sg</Text>
               </Link>
             </Box>
-            <Box h='100px' w='250px'>
+            <Box h='100px' w='270px'>
               <Text fontWeight='700' mb='20px'>
                 We are located at:
               </Text>
@@ -37,29 +37,27 @@ const Footer = () => {
               <Text>218 Pandan Loop, XPACE</Text>
               <Text>Singapore 128408</Text>
             </Box>
-            <Box h='100px' w='250px' display='flex' flexDirection='row' alignItems='end'>
+            <Box h='100px' w='170px' display='flex' flexDirection='row' alignItems='center'>
               <Text fontWeight='700'>Follow Us</Text>
             </Box>
           </Flex>
           <Stack direction='column' spacing='-1' pt='45px'>
-            <Text fontSize='sm' textAlign='center'>
-              Copyright Foodbank 2022. All rights reserved.
-            </Text>
+            <Text textAlign='center'>Copyright Foodbank 2022. All rights reserved.</Text>
             <Flex direction='row' align='center' justify='center'>
               <Link href='https://www.foodbank.sg/terms-of-service' isExternal textDecoration='none'>
-                <Text fontSize='sm' textAlign='center' px='1'>
+                <Text textAlign='center' px='1'>
                   Terms of Service
                 </Text>
               </Link>
               <Text>|</Text>
-              <Link href='https://www.foodbank.sg/privacy-policy' isExternal>
-                <Text fontSize='sm' textAlign='center' px='1'>
+              <Link href='https://www.foodbank.sg/privacy-policy' isExternal textDecoration='none'>
+                <Text textAlign='center' px='1'>
                   Privacy Policy
                 </Text>
               </Link>
               <Text>|</Text>
-              <Link href='https://www.foodbank.sg/board-policy' isExternal>
-                <Text fontSize='sm' textAlign='center' px='1'>
+              <Link href='https://www.foodbank.sg/board-policy' isExternal textDecoration='none'>
+                <Text textAlign='center' px='1'>
                   Board Policy
                 </Text>
               </Link>
