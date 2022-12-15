b74b274aeac6f1d877b6e7bb1c39abee9264974e
Stuart Long Chay Boon <chayboon@gmail.com>
2022-12-15 16:50:02 +0800

    feat: create input component

---
+++ components/Input.tsx
@@ -0,0 +1,21 @@
+const Input = ({ isDisabled = false, id, label }) => {
+  return (
+    <div className='w-2/6 text-left'>
+      <label htmlFor={id} className='mb-2 block text-sm text-gray-900	'>
+        {label}
+      </label>
+      <input
+        type='text'
+        id={id}
+        aria-label={id}
+        className={
+          (isDisabled ? 'bg-gray-100' : 'bg-white') +
+          ' mb-6 block w-full rounded-lg border border-gray-300 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500'
+        }
+        disabled={isDisabled}
+      />
+    </div>
+  );
+};
+
+export default Input;
--- pages/manage/index.tsx
+++ pages/manage/index.tsx
@@ -1,6 +1,7 @@
 import NavBarCart from '../../components/navbar/NavBarCart';
 import CustomButton from '../../components/Buttons';
 import { useState } from 'react';
+import Input from '../../components/Input';
 
 const Manage = () => {
   const [isDisabled, setIsDisabled] = useState(true);
@@ -10,45 +11,9 @@ const Manage = () => {
       <NavBarCart />
       <h1 className='my-10 text-center text-3xl font-bold'> Account Details </h1>
       <div className='align grid place-items-center'>
-        <label htmlFor='email' className='mb-2 block text-sm text-gray-900'>
-          Email
-        </label>
-        <input
-          type='text'
-          id='email'
-          aria-label='email'
-          className={
-            (isDisabled ? 'bg-gray-100' : 'bg-white') +
-            ' mb-6 block w-2/6 w-full rounded-lg border border-gray-300 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500'
-          }
-          disabled={isDisabled}
-        />
-        <label htmlFor='full-name' className='mb-2 block text-sm text-gray-900'>
-          Full Name
-        </label>
-        <input
-          type='text'
-          id='full-name'
-          aria-label='full-name'
-          className={
-            (isDisabled ? 'bg-gray-100' : 'bg-white') +
-            ' mb-6 block w-2/6 w-full rounded-lg border border-gray-300 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500'
-          }
-          disabled={isDisabled}
-        />
-        <label htmlFor='date-of-birth' className='mb-2 block text-sm text-gray-900'>
-          Date of Birth
-        </label>
-        <input
-          type='text'
-          id='date-of-birth'
-          aria-label='date-of-birth'
-          className={
-            (isDisabled ? 'bg-gray-100' : 'bg-white') +
-            ' mb-10 block w-2/6 w-full rounded-lg border border-gray-300 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500'
-          }
-          disabled={isDisabled}
-        />
+        <Input isDisabled={isDisabled} id='email' label='Email' />
+        <Input isDisabled={isDisabled} id='full-name' label='Full Name' />
+        <Input isDisabled={isDisabled} id='date-of-birth' label='Date of Birth' />
         <div>
           {isDisabled ? (
             <CustomButton className='w-full' variant='green-solid' onClick={() => setIsDisabled(!isDisabled)}>
