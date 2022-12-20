02eb61ce0b703cef611e6ecceb6df2367d5aac60
kxrt <kvrtikeya@gmail.com>
2022-12-20 17:30:48 +0530

    feat(view-cart): create responsible cart page
    - Style table colours as per prototype
    - Update handling of courses and prices for 'select all' box
    - Update table styling to match dimensions

--- pages/view-cart/index.tsx
+++ pages/view-cart/index.tsx
@@ -1,70 +1,124 @@
 import { useState } from 'react';
-import Head from 'next/head';
-import { Box, Flex, Text, Checkbox } from '@chakra-ui/react';
-import { Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, TableContainer } from '@chakra-ui/react';
+import { Box, Text, Checkbox } from '@chakra-ui/react';
+import { Table, Thead, Tbody, Tr, Th, Td, TableContainer } from '@chakra-ui/react';
 
 import Layout from '../../components/Layout';
 import styles from './ViewCart.module.css';
-import CustomButton from '../../components/Button';
+import Button from '../../components/Button';
 
 const ViewCart = () => {
   const courses = [
     {
       title: 'Food Sourcing',
-      description:
-        'Lorem ipsum dolor sit amet consectetur landitiis illum dolor. Am inventore natus, eos rerum om inventore natus, eos rerum. Tempore, sint at. Doloribus, odio eaque. Am inventore natus, eos rerum. Am inventore natus, eos rerum.',
-      duration: '10 hrs, 14 mins',
-      creator: 'Food Bank',
-      objective: 'This is one thing you could learn from this course.',
-      price: 2.95,
-      tags: ['#agriculture'],
-      category: 'Food Cycle',
+      price: 4.95,
+    },
+    {
+      title: 'Food Sourcing But Longer and Pricier',
+      price: 10.0,
     },
   ];
 
   const [checkedItems, setCheckedItems] = useState(courses.map(() => false));
   const allChecked = checkedItems.every(Boolean);
 
+  const handleRemove = course => {
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+    setCheckedItems([...checkedItems.slice(0, index), ...checkedItems.slice(index + 1)]);
+  };
+
   return (
-    <Layout>
-      <Head>
-        <title>View Cart</title>
-        <meta name='description' />
-      </Head>
+    <Layout title='View Cart'>
       <Box>
-        <Text className={styles.header} pb='55px' pt='35px' pl='110px'>
+        <Text className={styles.header} pb='55px' pt='35px' pl='160px'>
           My Cart
         </Text>
       </Box>
-      <TableContainer display='flex' maxW='60%' marginInline='20%'>
-        <Table variant='simple'>
-          <Thead>
-            <Tr>
-              <Th></Th>
-              <Th>Course Name</Th>
-              <Th>Price</Th>
-              <Th></Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {courses.map((course, index) => (
-              <Tr key={course.title}>
-                <Td>
-                  <Checkbox
-                    colorScheme='green'
-                    isChecked={checkedItems[index]}
-                    onChange={e => setCheckedItems([...checkedItems.slice(0, index), e.target.checked, ...checkedItems.slice(index + 1)])}
-                  />
-                </Td>
-                <Td>{course.title}</Td>
-                <Td>{course.price}</Td>
-                <Td>Remove</Td>
-              </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      </TableContainer>
+      <CartTable courses={courses} checkedItems={checkedItems} setCheckedItems={setCheckedItems} handleRemove={handleRemove} />
+      <TotalSummaryBox courses={courses} checkedItems={checkedItems} setCheckedItems={setCheckedItems} allChecked={allChecked} />
     </Layout>
+  );
+};
+
+const CartTable = ({ courses, checkedItems, setCheckedItems, handleRemove }) => {
+  return (
+    <TableContainer display='flex' maxW='60%' marginInline='20%' border='1px solid #B8DD72' borderRadius='lg'>
+      <Table variant='simple'>
+        <Thead bgColor='#EBF8D3'>
+          <Tr>
+            <Th></Th>
+            <Th>Course Name</Th>
+            <Th>Price</Th>
+            <Th></Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {courses.map((course, index) => (
+            <Tr key={course.title}>
+              <Td width='10%'>
+                <Checkbox
+                  colorScheme='green'
+                  isChecked={checkedItems[index]}
+                  onChange={e => setCheckedItems([...checkedItems.slice(0, index), e.target.checked, ...checkedItems.slice(index + 1)])}
+                />
+              </Td>
+              <Td width='70%'>{course.title}</Td>
+              <Td width='10%'>${course.price.toFixed(2)}</Td>
+              <Td width='10%'>
+                <Text
+                  fontSize='xs'
+                  color='red'
+                  onClick={() => handleRemove(course)}
+                  _hover={{ textDecoration: 'underline', cursor: 'pointer' }}
+                >
+                  Remove
+                </Text>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+const TotalSummaryBox = ({ courses, checkedItems, setCheckedItems, allChecked }) => {
+  return (
+    <Box
+      maxW='62%'
+      marginInline='19%'
+      marginTop='40px'
+      marginBottom='20px'
+      border='1px solid #B8DD72'
+      paddingBlock='5px'
+      paddingInline='20px'
+      display='flex'
+      flexDir={['column', 'column', 'row']}
+      justifyContent='space-between'
+      alignItems='center'
+    >
+      <Checkbox isChecked={allChecked} onChange={e => setCheckedItems(courses.map(() => e.target.checked))} colorScheme='green'>
+        Select All ({checkedItems.filter(Boolean).length})
+      </Checkbox>
+      <Box>
+        <Text fontSize='sm'>
+          Total ({checkedItems.filter(Boolean).length} course{checkedItems.filter(Boolean).length == 1 ? '' : 's'}):
+        </Text>
+        <Text fontWeight='700' fontSize='28px'>
+          $
+          {courses
+            .map((course, index) => (checkedItems[index] ? course.price : 0))
+            .reduce((a, b) => a + b, 0)
+            .toFixed(2)}
+        </Text>
+      </Box>
+      <Box display='flex' flexDir='row'>
+        <Button variant='black-solid' style={{ marginRight: '10px' }}>
+          Cancel
+        </Button>
+        <Button variant='green-solid'>Proceed</Button>
+      </Box>
+    </Box>
   );
 };
 
