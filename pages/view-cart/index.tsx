4f11be4af5be1159013b06ae2c19c7cbe748d76d
kxrt <kvrtikeya@gmail.com>
2022-12-24 21:06:30 +0530

    fix(view-cart): cart updating procedures
    - cart populates from local storage
    - cart saves to local storage on cancelling/proceeding
    - removal should update cart

--- pages/view-cart/index.tsx
+++ pages/view-cart/index.tsx
@@ -1,32 +1,57 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Box, Text, Checkbox } from '@chakra-ui/react';
 import { Table, Thead, Tbody, Tr, Th, Td, TableContainer } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 
 import Layout from '../../components/Layout';
 import styles from './ViewCart.module.css';
 import Button from '../../components/Button';
+import { Course } from '../../interfaces/index';
 
 const ViewCart = () => {
-  const courses = [
-    {
-      title: 'Food Sourcing',
-      price: 4.95,
-    },
-    {
-      title: 'Food Sourcing but Longer and Pricier',
-      price: 10.0,
-    },
-  ];
+  const router = useRouter();
 
-  const [cartCourses, setCartCourses] = useState(courses);
-
-  const [checkedItems, setCheckedItems] = useState(courses.map(() => false));
+  const [cartCourses, setCartCourses] = useState([]);
+  const [checkedItems, setCheckedItems] = useState([]);
   const allChecked = checkedItems.every(Boolean);
 
-  const handleRemove = course => {
-    const index = cartCourses.indexOf(course);
-    setCartCourses([...cartCourses.slice(0, index), ...cartCourses.slice(index + 1)]);
-    setCheckedItems([...checkedItems.slice(0, index), ...checkedItems.slice(index + 1)]);
+  useEffect(() => {
+    // dummy data
+    const courses = [
+      {
+        id: '1',
+        name: 'Food Sourcing',
+        description: 'Learn how to source food',
+        stars: 5,
+        adminId: '1',
+        price: 4.95,
+      },
+      {
+        id: '2',
+        name: 'Food Sourcing but Longer and Pricier',
+        description: 'Learn how to source food but Longer and Pricier',
+        stars: 4,
+        adminId: '2',
+        price: 10.0,
+      },
+    ];
+    setCartCourses(JSON.parse(localStorage.getItem('Cart')) || courses);
+    setCheckedItems(cartCourses.map(() => false));
+  }, []);
+
+  const handleRemove = index => {
+    setCartCourses(cartCourses.filter((course, i) => i !== index));
+    setCheckedItems(checkedItems.filter((item, i) => i !== index));
+  };
+
+  const handleProceed = () => {
+    localStorage.setItem('Cart', JSON.stringify(cartCourses.filter((course, index: number) => checkedItems[index])));
+    router.push('/apply-vouchers');
+  };
+
+  const handleCancel = () => {
+    localStorage.setItem('Cart', JSON.stringify(cartCourses.filter((course, index: number) => checkedItems[index])));
+    router.push('/');
   };
 
   return (
@@ -36,8 +61,21 @@ const ViewCart = () => {
           My Cart
         </Text>
       </Box>
-      <CartTable cartCourses={cartCourses} checkedItems={checkedItems} setCheckedItems={setCheckedItems} handleRemove={handleRemove} />
-      <TotalSummaryBox cartCourses={cartCourses} checkedItems={checkedItems} setCheckedItems={setCheckedItems} allChecked={allChecked} />
+      {cartCourses.length > 0 ? (
+        <>
+          <CartTable cartCourses={cartCourses} checkedItems={checkedItems} setCheckedItems={setCheckedItems} handleRemove={handleRemove} />
+          <TotalSummaryBox
+            cartCourses={cartCourses}
+            checkedItems={checkedItems}
+            setCheckedItems={setCheckedItems}
+            allChecked={allChecked}
+            handleProceed={handleProceed}
+            handleCancel={handleCancel}
+          />
+        </>
+      ) : (
+        <EmptyCart />
+      )}
     </Layout>
   );
 };
@@ -55,8 +93,8 @@ const CartTable = ({ cartCourses, checkedItems, setCheckedItems, handleRemove })
           </Tr>
         </Thead>
         <Tbody>
-          {cartCourses.map((course, index) => (
-            <Tr key={course.title} className={styles.tablebody}>
+          {cartCourses.map((course, index: number) => (
+            <Tr key={course.id} className={styles.tablebody}>
               <Td width='10%'>
                 <Checkbox
                   colorScheme='green'
@@ -64,13 +102,13 @@ const CartTable = ({ cartCourses, checkedItems, setCheckedItems, handleRemove })
                   onChange={e => setCheckedItems([...checkedItems.slice(0, index), e.target.checked, ...checkedItems.slice(index + 1)])}
                 />
               </Td>
-              <Td width='70%'>{course.title}</Td>
+              <Td width='70%'>{course.name}</Td>
               <Td width='10%'>${course.price.toFixed(2)}</Td>
               <Td width='10%'>
                 <Text
                   fontSize='xs'
                   color='red'
-                  onClick={() => handleRemove(course)}
+                  onClick={() => handleRemove(index)}
                   _hover={{ textDecoration: 'underline', cursor: 'pointer' }}
                 >
                   Remove
@@ -84,7 +122,7 @@ const CartTable = ({ cartCourses, checkedItems, setCheckedItems, handleRemove })
   );
 };
 
-const TotalSummaryBox = ({ cartCourses, checkedItems, setCheckedItems, allChecked }) => {
+const TotalSummaryBox = ({ cartCourses, checkedItems, setCheckedItems, allChecked, handleProceed, handleCancel }) => {
   return (
     <Box
       maxW='62%'
@@ -100,8 +138,8 @@ const TotalSummaryBox = ({ cartCourses, checkedItems, setCheckedItems, allChecke
       alignItems='center'
       className={styles.tablebody}
     >
-      <Checkbox isChecked={allChecked} onChange={e => setCheckedItems(cartCourses.map(() => e.target.checked))} colorScheme='green'>
-        Select All ({checkedItems.length})
+      <Checkbox isChecked={allChecked} onChange={e => setCheckedItems(checkedItems.map(() => e.target.checked))} colorScheme='green'>
+        Select All ({cartCourses.length})
       </Checkbox>
       <Box>
         <Text fontSize='sm'>
@@ -110,19 +148,25 @@ const TotalSummaryBox = ({ cartCourses, checkedItems, setCheckedItems, allChecke
         <Text fontWeight='700' fontSize='28px'>
           $
           {cartCourses
-            .map((course, index) => (checkedItems[index] ? course.price : 0))
-            .reduce((a, b) => a + b, 0)
+            .map((course, index: number) => (checkedItems[index] ? course.price : 0))
+            .reduce((a: number, b: number) => a + b, 0)
             .toFixed(2)}
         </Text>
       </Box>
       <Box display='flex' flexDir='row'>
-        <Button variant='black-solid' style={{ marginRight: '10px' }}>
+        <Button variant='black-solid' style={{ marginRight: '10px' }} onClick={handleCancel}>
           Cancel
         </Button>
-        <Button variant='green-solid'>Proceed</Button>
+        <Button variant='green-solid' onClick={handleProceed}>
+          Proceed
+        </Button>
       </Box>
     </Box>
   );
+};
+
+const EmptyCart = () => {
+  return <>Your cart is empty.</>;
 };
 
 export default ViewCart;
