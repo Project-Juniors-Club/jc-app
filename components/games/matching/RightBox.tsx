265c787bb41454a8d997b481545a88eead86299c
Ambrose <ambroseliew1998@gmail.com>
2023-01-11 13:16:19 +0800

    Have line now

--- components/games/matching/RightBox.tsx
+++ components/games/matching/RightBox.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const RightBox = ({ name, handleMouseUp }) => {
+const RightBox = ({ name }) => {
   const style = {
     width: '100px',
     height: '100px',
@@ -9,7 +9,7 @@ const RightBox = ({ name, handleMouseUp }) => {
   };
 
   return (
-    <button style={style} className='my-10 mx-20' onMouseUp={handleMouseUp}>
+    <button style={style} className='my-10 mx-20' data-column={'right'} data-name={name}>
       {name}
     </button>
   );
