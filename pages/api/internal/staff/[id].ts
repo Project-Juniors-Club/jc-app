74a50bcff1099c433ab01e3efc61b2f5a6e60b4d
Yu Shi Jie <yshijie1999@gmail.com>
2023-11-01 21:30:26 +0800

    feat: first commit into branch

--- pages/api/internal/staff/[id].ts
+++ pages/api/internal/staff/[id].ts
@@ -2,10 +2,23 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../../lib/prisma';
 import { entityMessageCreator } from '../../../../utils/api-messages';
 import { errorMessageHandler } from '../../../../utils/error-message-handler';
+import { getSession } from 'next-auth/react';
+import { UserType } from '@prisma/client';
 
 const entityMessageObj = entityMessageCreator('admin');
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const session = await getSession({ req });
+  console.log(JSON.stringify(session));
+
+  // Verify user is an admin
+  if (session.user?.type !== UserType.admin) {
+    res.status(200).json({
+      error: 'You must be an admin to use this API.',
+      session: JSON.stringify(session)
+    })
+  }
+
   const httpMethod = req.method;
   const id = req.query.id as string;
 
@@ -26,6 +39,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         },
       });
       res.status(200).json({ message: entityMessageObj.getOneSuccess, data: admin });
+    } else if (httpMethod == 'DELETE') {
+      const deleteUser = await prisma.user.delete({
+        where: {
+          id: id,
+        },
+      });
+      res.status(200).json({ message: entityMessageObj.deleteSuccess, data: deleteUser });
     } else if (httpMethod == 'PUT') {
       const { role, name, disabled } = req.body;
       const updatedUser = await prisma.admin.update({
