e8bb7f1d9c622a132b16f7cdc8098561019ab81b
sltsheryl <sltsheryl@gmail.com>
2024-02-12 17:54:15 +0800

    fix typo

--- pages/api/courses/categories/index.ts
+++ pages/api/courses/categories/index.ts
@@ -17,7 +17,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           },
         },
       });
-      res.status(200).json({ messgae: entityMessageObj.getAllSuccess, data: categories });
+      res.status(200).json({ message: entityMessageObj.getAllSuccess, data: categories });
     } else if (httpMethod == 'POST') {
       // Create Cateogry
       const { name, description } = req.body;
