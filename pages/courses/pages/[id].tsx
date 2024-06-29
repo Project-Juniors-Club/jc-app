cc88615a089ffe20e82cbd94612d96e50c5d9833
sltsheryl <sltsheryl@gmail.com>
2024-06-30 01:35:23 +0800

    fix: eslint warning

--- pages/courses/pages/[id].tsx
+++ pages/courses/pages/[id].tsx
@@ -1,7 +1,18 @@
 import axios from 'axios';
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
-import { Box, Text, Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon, Checkbox, Flex, Button } from '@chakra-ui/react';
+import {
+  Box,
+  Text,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Checkbox,
+  Flex,
+  Button,
+} from '@chakra-ui/react';
 import { getSession } from 'next-auth/react';
 import { getUserCourseId } from '../../../lib/server/userCourses';
 import styles from '../../../components/Course.module.css';
@@ -12,7 +23,7 @@ import Layout from '../../../components/Layout';
 import NavBarCourse from '../../../components/navbar/NavBarCourse';
 
 export default function Page({ userCourseId, courseChapters, course, chapter, page, article }) {
-  const { chapters } = courseChapters
+  const { chapters } = courseChapters;
   const [chapterCompletionStatus, setChapterCompletionStatus] = useState({});
   const [pageCompletionStatus, setPageCompletionStatus] = useState({});
   useEffect(() => {
@@ -27,23 +38,42 @@ export default function Page({ userCourseId, courseChapters, course, chapter, pa
   }, [userCourseId, chapters]);
 
   return (
-  <Layout title={course.title}>
+    <Layout title={course.title}>
       <NavBarCourse />
-    
+
       <Head>
-         <title>{page.name}</title>
+        <title>{page.name}</title>
       </Head>
-      <Flex direction="column" p="20px">
-        <Text fontSize="sm" color="gray.600">{course.title} > {chapter.name}</Text>
-        <Flex justifyContent="space-between" alignItems="center" mt="10px">
-          <Button background="transparent" onClick={() => {/* handle previous */}}> &lt; &nbsp; Previous</Button>
-          <Button background="transparent" onClick={() => {/* handle next */}}>Next &nbsp; &gt; </Button>
+      <Flex direction='column' p='20px'>
+        <Text fontSize='sm' color='gray.600'>
+          {course.title} `{'>'}` {chapter.name}
+        </Text>
+        <Flex justifyContent='space-between' alignItems='center' mt='10px'>
+          <Button
+            background='transparent'
+            onClick={() => {
+              /* handle previous */
+            }}
+          >
+            {' '}
+            &lt; &nbsp; Previous
+          </Button>
+          <Button
+            background='transparent'
+            onClick={() => {
+              /* handle next */
+            }}
+          >
+            Next &nbsp; &gt;{' '}
+          </Button>
         </Flex>
-        <Flex mt="20px">
-          <Box width="25%" pr="20px">
-           <Text mb="15px" fontWeight="bold" style={{ fontSize: '32px' }}>{course.title}</Text>
-             <Accordion allowMultiple className={styles.accordion}>
-               {chapters.map((chapter, chapterIndex) => (
+        <Flex mt='20px'>
+          <Box width='25%' pr='20px'>
+            <Text mb='15px' fontWeight='bold' style={{ fontSize: '32px' }}>
+              {course.title}
+            </Text>
+            <Accordion allowMultiple className={styles.accordion}>
+              {chapters.map((chapter, chapterIndex) => (
                 <AccordionItem key={chapterIndex} className='bg-main-light-green'>
                   <h2>
                     <AccordionButton border='1px solid #C7C7C7'>
@@ -83,15 +113,16 @@ export default function Page({ userCourseId, courseChapters, course, chapter, pa
               ))}
             </Accordion>
           </Box>
-          <Box width="75%" pl="20px">
-            <Text mb="15px" fontWeight="bold" fontSize="32px">{page.name}</Text>
-            <Text mb="10px" fontSize="20px">
-             {article.text}
+          <Box width='75%' pl='20px'>
+            <Text mb='15px' fontWeight='bold' fontSize='32px'>
+              {page.name}
+            </Text>
+            <Text mb='10px' fontSize='20px'>
+              {article.text}
             </Text>
           </Box>
         </Flex>
       </Flex>
-     
     </Layout>
   );
 }
@@ -109,17 +140,18 @@ export const getServerSideProps: GetServerSideProps = async context => {
         notFound: true,
       };
     }
-     const courseId = data.course.id;
+    const courseId = data.course.id;
 
-  let userCourseId = await getUserCourseId(userId, courseId);
-  if (userCourseId === undefined) {
-    userCourseId = null;
-  }
+    let userCourseId = await getUserCourseId(userId, courseId);
+    if (userCourseId === undefined) {
+      userCourseId = null;
+    }
     const courseChapters = await getCourseContentOverview(courseId);
-    
+
     return {
       props: {
-        userCourseId, courseChapters,
+        userCourseId,
+        courseChapters,
         ...data,
       },
     };
