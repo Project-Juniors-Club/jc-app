e238b21678a459b69ee1b91c41cf1057da03e4a0
miloaisdino <miloaisdino@gmail.com>
2024-12-18 12:45:50 +0800

    fix: change user list to serversideprops

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps, GetStaticPaths } from 'next';
+import { GetServerSideProps } from 'next';
 
 import { User } from '../../interfaces';
 import Layout from '../../components/Layout';
@@ -10,49 +10,30 @@ type Props = {
   errors?: string;
 };
 
-const StaticPropsDetail = ({ item, errors }: Props) => {
+const ServerSidePropsDetail = ({ item, errors }: Props) => {
   if (errors) {
     return (
-      <Layout title='Error | Next.js + TypeScript Example'>
-        <p>
-          <span style={{ color: 'red' }}>Error:</span> {errors}
-        </p>
-      </Layout>
+        <Layout title='Error | Next.js + TypeScript Example'>
+          <p>
+            <span style={{ color: 'red' }}>Error:</span> {errors}
+          </p>
+        </Layout>
     );
   }
 
   return <Layout title={`${'User Detail'} | Next.js + TypeScript Example`}>{item && <ListDetail item={item} />}</Layout>;
 };
 
-export default StaticPropsDetail;
+export default ServerSidePropsDetail;
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  // Get the paths we want to pre-render based on users
-  const users = await prisma.user.findMany();
-  const paths = users.map(user => ({
-    params: { id: user.id.toString() },
-  }));
-
-  // We'll pre-render only these paths at build time.
-  // { fallback: false } means other routes should 404.
-  return { paths, fallback: false };
-};
-
-// This function gets called at build time on server-side.
-// It won't be called on client-side, so you can even do
-// direct database queries.
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   try {
     const id = params?.id;
-    const users = await prisma.user.findMany();
-    const item = users.find(data => data.id === id);
     const user = await prisma.user.findFirst({
       where: {
-        id: params?.id.toString(),
+        id: id?.toString(),
       },
     });
-    // By returning { props: item }, the StaticPropsDetail component
-    // will receive `item` as a prop at build time
     return { props: { item: JSON.parse(JSON.stringify(user)) } };
   } catch (err: any) {
     return { props: { errors: err.message } };
